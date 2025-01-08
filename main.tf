e6cae660fd436b7eae8e3d732caaf05a0cd73c4b
Andrew Hatch <hatchman76@gmail.com>
2025-01-08 15:09:38 -0800

    using module for security group

--- main.tf
+++ main.tf
@@ -19,13 +19,25 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "5.3.0"
+  vpc_id              = data.aws_vpc.default.id
+  name                = "blog_new"  
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
@@ -63,3 +75,4 @@ resource "aws_security_group_rule" "blog_everything_out" {
 
   security_group_id = aws_security_group.blog.id
 }
+
