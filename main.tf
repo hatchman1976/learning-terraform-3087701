42945bc980a113a9cca1cfb9090ed896c6aedb2f
Andrew Hatch <hatchman76@gmail.com>
2025-01-09 14:40:45 -0800

    remove alb target

--- main.tf
+++ main.tf
@@ -60,16 +60,6 @@ module "blog_alb" {
 
   security_groups               = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
   listeners = {
     ex-http = {
         port     = 80
