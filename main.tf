9a762658e0756f75f0a0692cbfe8eacaa45b17f2
Andrew Hatch <hatchman76@gmail.com>
2025-01-08 16:38:59 -0800

    load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,39 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name                          = "blog-alb"
+  vpc_id                        = module.blog_vpc.vpc_id
+  subnets                       = module.blog_vpc.public_subnets
+
+  security_groups               = module.blog_sg.security_group_id 
+
+  listeners = {
+    ex-http-tcp = {
+      port     = 80
+      protocol = "HTTP"
+    }
+    
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
