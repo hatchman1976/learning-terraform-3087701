75bb7150c52bd08ed9402632f220d46ebe0c9afb
Andrew Hatch <hatchman76@gmail.com>
2025-01-08 17:19:13 -0800

    more updates

--- main.tf
+++ main.tf
@@ -64,13 +64,15 @@ module "alb" {
     }
   }
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  listeners = {
+    ex-http = {
+        port     = 80
+        protocol = "HTTP"
+        forward = {
+          target_group_key = "ex-instance"
+        }
+      }
     }
-  ]
 
   tags = {
     Environment = "dev"
