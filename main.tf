8571a732cb56e721fa00e5ad55735646b0d2bbd6
Andrew Hatch <hatchman76@gmail.com>
2025-01-08 16:51:29 -0800

    trouble

--- main.tf
+++ main.tf
@@ -54,24 +54,6 @@ module "alb" {
 
   security_groups               = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-tcp = {
-      port     = 80
-      protocol = "HTTP"
-    }
-    
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
   tags = {
     Environment = "Development"
     Project     = "Example"
