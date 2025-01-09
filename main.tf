91b4480977ba8019d0f4e6fb639801d06abc4390
Andrew Hatch <hatchman76@gmail.com>
2025-01-09 14:46:11 -0800

    test

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.blog_asg.blog.id
+      target_id        = module.blog_asg.id
     }
   }
 
