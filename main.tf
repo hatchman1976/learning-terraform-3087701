afa153a33723dbf03288f315204d17cbd68a35a1
Andrew Hatch <andrewhatch@Andrews-MacBook-Pro.local>
2025-01-10 09:55:27 -0800

    test

--- main.tf
+++ main.tf
@@ -40,7 +40,20 @@ resource "aws_autoscaling_group" "blog_asg" {
 
   target_group_arns = [aws_lb_target_group.blog_alb_tg.arn]
 
+  launch_configuration = aws_launch_configuration.as_conf
+
 }
+
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = data.aws_ami.app_ami
+  instance_type = "t2.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
