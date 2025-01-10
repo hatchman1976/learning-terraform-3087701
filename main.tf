a7f0be8c4e8bf3b2a08be57b35598dbaeb88ee38
Andrew Hatch <hatchman76@gmail.com>
2025-01-09 16:32:19 -0800

    simple

--- main.tf
+++ main.tf
@@ -67,8 +67,10 @@ module "blog_vpc" {
 module "blog-alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.id
+  name                = "blog-alb"
+  vpc_id              = module.blog_vpc.vpc_id
+  subnets             = module.blog_vpc.public_subnets
+  security_groups     = [module.blog_sg.security_group_id]
 
   tags = {
     Environment = "Development"
