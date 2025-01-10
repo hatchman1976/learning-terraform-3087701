c3b750e4d768ac9daaf31cb19954eec0142800f5
Andrew Hatch <hatchman76@gmail.com>
2025-01-09 16:15:06 -0800

    removed the ALB

--- main.tf
+++ main.tf
@@ -47,31 +47,6 @@ module "blog_asg" {
 
 
 
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "9.13.0"
-
-  name                          = "blog-alb"
-  
-  load_balancer_type            = "application"
-  
-  vpc_id                        = module.blog_vpc.vpc_id
-  subnets                       = module.blog_vpc.public_subnets
-
-  security_groups               = [module.blog_sg.security_group_id]
-
-  listeners = {
-    ex-http = {
-        port     = 80
-        protocol = "HTTP"
-      }
-    }
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
